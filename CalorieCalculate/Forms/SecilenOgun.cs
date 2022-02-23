36e3489466c17b7b29fd30f0c6f8c8ed79705c1b
unknown <altayneset@gmail.com>
2022-02-23 05:00:21 +0300

    SecilenOgunList

--- CalorieCalculate/Forms/SecilenOgun.cs
+++ CalorieCalculate/Forms/SecilenOgun.cs
@@ -1,4 +1,5 @@
-﻿using CalorieCalculate.Model.Entities;
+﻿using CalorieCalculate.Model.Data;
+using CalorieCalculate.Model.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,6 +25,25 @@ namespace CalorieCalculate.Forms
         {
             this.repast = repast;
             lblOgunAdi.Text = repast.RepastName;
+        }
+
+        private void Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            switch (btn.Tag.ToString())
+            {
+                case "1":
+                    YemekListele();
+                    break;
+            }
+        }
+
+        private void YemekListele()
+        {
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                var yemekler = db.Meals.Select(x => new { Id = x.Id, MealName = x.MealName, Description = x.MealDescription, Calorie = x.Calorie, TypMeal = x.TypeMealId }).ToList();
+            }
         }
     }
 }
